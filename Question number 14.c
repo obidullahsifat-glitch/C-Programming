de6415ab0fa188bd84b65b09d9c603526040a065
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-08-01 18:31:47 +0600

    Add files via upload

---
+++ Question number 14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int var= 2;   //global variable
+int main()
+{
+printf("%i\n", var*var);   //output 4
+ int var= 3;    // local variable
+ {
+ int var= 5;   //local variable
+ printf("%i\n", var * var);    //output 25
+
+
+ }
+
+printf("%i\n", var * var);   //local variable , output 9
+
+return 0;
+}
